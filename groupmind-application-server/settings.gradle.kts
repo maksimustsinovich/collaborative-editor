c310260de6e72c9e09060657e68848370e1ea375
Maksim <maksimustsinovich@gmail.com>
2024-04-28 09:15:44 +0300

    Initialize application server project

---
+++ groupmind-application-server/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "groupmind-application-server"
---
+++ groupmind-application-server/src/test/java/by/ustsinovich/groupmind/GroupmindApplicationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.ustsinovich.groupmind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupmindApplicationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
